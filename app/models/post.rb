8c2d695584cc632f8c9c6e39803c652703e9e889
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-20 16:55:33 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true, inclusion: { in: ["Won't Believe","Secret","Top","Guess"}
+  validates :title, presence: true, inclusion: { in: ["Won't Believe","Secret","Top","Guess"]}
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}

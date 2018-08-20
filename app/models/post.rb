b3f7f61d3567ab576008727daf6e14f0ab61203b
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-20 17:09:18 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,7 @@ end
 
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
-  
+
   validates :title, presence: true, validates_with MyValidator
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}

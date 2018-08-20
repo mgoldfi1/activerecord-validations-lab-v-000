14ef8bef59e950b773409fe3faf4d5c21a066e47
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-20 17:57:46 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,7 +14,7 @@ validate :is_clickbait?
   def is_clickbait?
   cb = [/Won't Believe/i,/Top [0-9]*/i,/Guess/i,/Secret/i]
   if cb.none? {|x| x.match(self.title)}
-    binding.pry
+    errors.add(:title, "must be clickbait")
   end
   end
 

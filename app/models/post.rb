28484c93281947d38c705af3bbaf83ff4ede1a3c
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-20 17:32:18 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,16 @@
 
 
-class MyValidator < ActiveModel::Validator
-  def validate(post)
-    unless post.title["Won't Believe"]
-      record.errors[:title] << 'Need a  clickbait title'
-    end
-  end
-end
-
 
 
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations
-  validates_with MyValidator
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+  cb = ["Won't Believe", "Top", "Guess", "Secret"]
+  def is_clickbait?
+  end
+
+
 end

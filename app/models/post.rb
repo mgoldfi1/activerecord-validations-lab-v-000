d72a7843f15ebc99a97f98b95b2d9c9de9f0dc82
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-20 17:42:30 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,13 +5,12 @@ require 'pry'
 class Post < ActiveRecord::Base
 
   validates :title, presence: true
-  validate :is_clickbait?
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 
 
-
+validate :is_clickbait?
   def is_clickbait?
     binding.pry
   cb = ["Won't Believe", "Top", "Guess", "Secret"]

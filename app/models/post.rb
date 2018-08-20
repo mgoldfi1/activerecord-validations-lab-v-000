86135f2ff0c41253caeb99edffd452cb8b4a31d8
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-20 17:09:38 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,8 +12,8 @@ end
 
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
-
-  validates :title, presence: true, validates_with MyValidator
+  validates_with MyValidator
+  validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}

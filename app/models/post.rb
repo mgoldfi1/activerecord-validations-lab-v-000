6a869bc8ede7aa17a8c932c86f852f683fa5c9db
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-20 16:56:45 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
+
+
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.name.starts_with? 'X'
+      record.errors[:name] << 'Need a name starting with X please!'
+    end
+  end
+end
+
+
+
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}

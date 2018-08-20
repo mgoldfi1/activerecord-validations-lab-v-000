f23cc5ee37d21c3624289ca082f85a97745e0a34
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-20 17:39:22 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,9 @@
 
 
 class Post < ActiveRecord::Base
-  validate :is_clickbait?
+
   validates :title, presence: true
+  validate :is_clickbait?
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}

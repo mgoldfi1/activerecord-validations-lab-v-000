6c7c5c98903881f0052151b282912f3b826fd9f5
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-20 17:58:32 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
+
+
+require 'pry'
+
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+
+validate :is_clickbait?
+  def is_clickbait?
+  cb = [/Won't Believe/i,/Top [0-9]*/i,/Guess/i,/Secret/i]
+  if cb.none? {|x| x.match(self.title)}
+    errors.add(:title, "must be clickbait")
+  end
+  end
+
+
 end
